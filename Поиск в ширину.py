4df37c8ad26d03a67f534fc623310d4a0f072191
nameEricsson <battekfild@gmail.com>
2023-03-15 21:16:04 +0500

    Create Поиск в ширину.py

---
+++ Поиск в ширину.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+graph = {'you': ['alice', 'bob', 'claire', 'moris', 'jeim'],
+         'person':[]
+         }
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + ' is a mango seller!')
+                return True
+            else:
+                search_queue += graph['person']
+                searched.append(person)
+    return False
+search('you')
+
+
+
+
